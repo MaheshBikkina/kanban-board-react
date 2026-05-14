862412aa74bc55d5dd26edb0c8b575fa6781d22a
MaheshBikkina <maheshbikkina3310@gmail.com>
2026-05-14 13:17:16 +0530

    Configure GitHub Pages deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'kanban-board-react',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
